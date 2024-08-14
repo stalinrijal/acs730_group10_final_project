07f0c986848324f38951d203ce43d1a76b08dd34
Stalin Rijal <stalinrijal@gmail.com>
2024-08-14 15:38:17 -0400

    adding terraform files for network and web servers

---
+++ terraform/prod/webservers/config.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "prod-bucket-sgaire3"            // Bucket from where to GET Terraform State
+    key    = "prod/webservers/terraform.tfstate" // Object name in the bucket to GET Terraform State
+    region = "us-east-1"                         // Region where bucket created
+  }
+}
