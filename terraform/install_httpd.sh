bd0686d4121b54e37a3819d8c0dba9c06d207310
Stalin Rijal <stalinrijal@gmail.com>
2024-08-16 20:15:18 -0400

    fix webserver

--- terraform/install_httpd.sh
+++ terraform/install_httpd.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
+
+# Download image from S3
 aws s3 cp s3://acs730-group10-bucket/images/sample.jpg /var/www/html/image.jpg
-myip='curl http://169.254.169.254/latest/meta-data/local-ipv4'
-echo '<h1>Welcome to ACS730 Final project! Group 10: : Stalin, Anup, Bhupendra --- My private IP is '"$myip"' <font color="turquoise"> in prod environment</font></h1><br>Built by Terraform Group 10!' > /var/www/html/index.html
+
+# Retrieve the instance's private IP address
+myip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Create an HTML page with IP and image
+echo '<h1>Welcome to ACS730 Final project! Group 10: Stalin, Anup, Bhupendra --- My private IP is '"$myip"' <font color="turquoise"> in prod environment</font></h1><br><img src="image.jpg" alt="Group 10 Image"><br>Built by Terraform Group 10!' > /var/www/html/index.html
+
+# Start and enable Apache HTTP server
 sudo systemctl start httpd
 sudo systemctl enable httpd
